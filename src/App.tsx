c665e6644d4b45eff21684c6634782074662ad6c
Osman Piska <osman_piska@hotmail.com>
2021-04-01 22:47:50 -0300

    DESAFIOS COMPLEMENTARES

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+import './styles/global.scss';
+
+import { SideBar } from './components/SideBar';
+
+import { Content } from './components/Content';
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar selectedGenreId={selectedGenreId} setSelectedGenreId={setSelectedGenreId} />
+      <Content selectedGenreId={selectedGenreId} />
+    </div>
+  )
+}
